0aed287390d3f1a4a81251adb7372956f15f0299
mleanos <michael.leanos@gmail.com>
2017-09-05 20:24:22 -0700

    feat(shortcuts): Filtering & Paging
    Adds client-side filtering and paging to the Shortcuts list page.

--- modules/shortcuts/client/controllers/list-shortcuts.client.controller.js
+++ modules/shortcuts/client/controllers/list-shortcuts.client.controller.js
@@ -5,12 +5,41 @@
     .module('shortcuts')
     .controller('ShortcutsListController', ShortcutsListController);
 
-  ShortcutsListController.$inject = ['ShortcutsService', 'Authentication'];
+  ShortcutsListController.$inject = ['ShortcutsService', 'Authentication', '$filter'];
 
-  function ShortcutsListController(ShortcutsService, Authentication) {
+  function ShortcutsListController(ShortcutsService, Authentication, $filter) {
     var vm = this;
     vm.user = Authentication.user;
 
-    vm.shortcuts = ShortcutsService.query();
+    vm.buildPager = buildPager;
+    vm.figureOutItemsToDisplay = figureOutItemsToDisplay;
+    vm.pageChanged = pageChanged;
+
+    ShortcutsService.query(function (data) {
+      vm.shortcuts = data;
+      vm.buildPager();
+    });
+
+    function buildPager() {
+      vm.pagedItems = [];
+      vm.itemsPerPage = 20;
+      vm.currentPage = 1;
+      vm.figureOutItemsToDisplay();
+    }
+
+    function figureOutItemsToDisplay() {
+      vm.filteredItems = $filter('filter')(vm.shortcuts, {
+        $: vm.search
+      });
+
+      vm.filterLength = vm.filteredItems.length;
+      var begin = ((vm.currentPage - 1) * vm.itemsPerPage);
+      var end = begin + vm.itemsPerPage;
+      vm.pagedItems = vm.filteredItems.slice(begin, end);
+    }
+
+    function pageChanged() {
+      vm.figureOutItemsToDisplay();
+    }
   }
 }());
