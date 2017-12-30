e62c27abf08b66c0d3fb48b3db56948f073a6bc0
Samer Masterson <nosefrog@gmail.com>
2017-12-30 10:02:46 -0800

    Keep trying to reconnect to the database in prod
    Should prevent the "Topology was destroyed" outage that we saw this morning.
    
    More info: https://stackoverflow.com/a/39831825

--- config/env/production.js
+++ config/env/production.js
@@ -30,6 +30,11 @@ module.exports = {
       sslPass: '1234'
 
       */
+      reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+      reconnectInterval: 500, // Reconnect every 500ms
+      poolSize: 10, // Maintain up to 10 socket connections
+      // If not connected, return errors immediately rather than waiting for reconnect
+      bufferMaxEntries: 0
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
